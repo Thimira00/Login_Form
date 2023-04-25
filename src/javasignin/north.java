336bae6f552bc953104a0120c427ff08e0235061
Thimira00 <121315470+Thimira00@users.noreply.github.com>
2023-04-25 23:53:28 +0530

    finished

---
+++ src/javasignin/north.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasignin;
+
+import java.awt.Cursor;
+
+/**
+ *
+ * @author Ushan
+ */
+class north {
+
+    static void setCursor(Cursor cur1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
